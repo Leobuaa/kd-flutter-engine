bdc42dea1884c742d3769be31d0b46327cbfd3b7
leopeng <leopeng@tencent.com>
2021-11-15 11:13:38 +0800

    [U] 调整编译脚本

--- kd_flutter_frontend_server/compile.sh
+++ kd_flutter_frontend_server/compile.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 # compile flutter_frontend_server.dart.snapshot
-dart --snapshot-kind=kernel --snapshot=frontend_server.dart.snapshot --packages=.dart_tool/package_config.json   bin/starter.dart
+# 修改FLUTTER_HOME为对应的flutter sdk目录
+FLUTTER_HOME="/Users/pengzimao/Documents/leo_project/flutter2"
+$FLUTTER_HOME/bin/dart --snapshot-kind=kernel --snapshot=frontend_server.dart.snapshot --packages=.dart_tool/package_config.json  bin/starter.dart
